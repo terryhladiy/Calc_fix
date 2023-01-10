288414a6d0d42358a7bcab54f70f6a45ce0eee46
taras <tarik2002sta@gmail/com>
2023-01-10 02:40:29 +0200

    FIRST JOB

---
+++ script.js
@@ -0,0 +1,20 @@
+alert('Калькулятор');
+
+const whatDo = (prompt('add (+), sub (-), mult (/), div(*)'));
+
+const firstNumber = +(prompt('Вкажіть першу цифру'));
+
+const secondNumber = +(prompt('Вкажіть другу цифру'));
+if(whatDo == '-' || whatDo == 'sub' ){
+    sum = firstNumber - secondNumber;
+    alert('А ось і ваш результат : '+firstNumber +'-' + secondNumber + '=' + sum);
+}else if(whatDo == '+' || whatDo == 'add'){
+    sum = firstNumber + secondNumber;
+    alert('А ось і ваш результат : '+firstNumber +'+' + secondNumber + '=' + sum);
+}else if(whatDo == '*' || whatDo == 'mult'){
+    sum = firstNumber * secondNumber;
+    alert('А ось і ваш результат : '+firstNumber +'*' + secondNumber + '=' + sum);
+}else if(whatDo == '/' || whatDo == 'div'){
+    sum = firstNumber / secondNumber;
+    alert('А ось і ваш результат : '+firstNumber +'/' + secondNumber + '=' + sum);
+}
